d371b131d15f2088bc41578f5ae0df16a3170386
Kidy Lee <kidylee@gmail.com>
2018-03-15 21:42:51 -0400

    Initial Commit

---
+++ beatcoin/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  beatcoin
+//
+//  Created by An Li on 3/15/18.
+//  Copyright © 2018 An Li. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
